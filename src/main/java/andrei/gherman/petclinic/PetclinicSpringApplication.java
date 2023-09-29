38023def484a5ccfc19b620712859c12532a0cf0
Andrei <andrei30.gherman@gmail.com>
2023-09-29 18:24:40 +0300

    first commit

---
+++ src/main/java/andrei/gherman/petclinic/PetclinicSpringApplication.java
@@ -0,0 +1,13 @@
+package andrei.gherman.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/andrei/gherman/petclinic/PetclinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package andrei.gherman.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
