b64dd02ac245d456a1d79855d02ff04f60920f42
Andrei <andrei30.gherman@gmail.com>
2023-10-09 16:16:01 +0300

    Common crud Interface. closes #10

--- pet-clinic-data/src/main/java/andrei/gherman/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/andrei/gherman/petclinic/services/PetService.java
@@ -3,12 +3,7 @@ package andrei.gherman.petclinic.services;
 
 import andrei.gherman.petclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long>{
 
-    Pet findById(Long id);
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
